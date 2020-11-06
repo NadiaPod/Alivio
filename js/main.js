1fbcaea8cb5ec14151f11956bf0d7c1f3a94ba17
NadiaPod <72332868+NadiaPod@users.noreply.github.com>
2020-11-06 16:08:00 +0300

    Work in progress
    Work in progress

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 window.onload = function(){
+	let body = document.querySelector('body');
+	let container = document.querySelector('.container');
 	let header__burger = document.querySelector('.header__burger');
 	let header__menu = document.querySelector('.header__menu');
 	let header__nav = document.querySelector('.header__nav');
@@ -6,19 +8,25 @@ window.onload = function(){
 	let header__btn = document.querySelector('.header__btn');
 
 	header__burger.onclick = function(){
+		body.classList.toggle('shown');
+		container.classList.toggle('shown');
 	    header__burger.classList.toggle('shown');
 	    header__menu.classList.toggle('shown');
-	}
+	};
 
 	header__nav.onclick = function () {
+		body.classList.toggle('shown');
+		container.classList.remove('shown');
 		header__burger.classList.remove('shown');
 	    header__menu.classList.remove('shown');
-	}
+	};
 
 	header__btn.onclick = function() {
+		body.classList.remove('shown');
+		container.classList.remove('shown');
 		header__burger.classList.remove('shown');
 	    header__menu.classList.remove('shown');
-	}
+	};
 
 	let login = document.querySelector('.login');
 	let login__form = document.querySelector('.login__form');
@@ -26,20 +34,22 @@ window.onload = function(){
 	let login__exit = document.querySelector('.login__exit');
 
 	header__signIn.onclick = function(){
+		container.classList.remove('shown');
 		header__burger.classList.remove('shown');
 	    header__menu.classList.remove('shown');
 	    login.classList.toggle('active');
-	}
+	};
 
 	login__btn.onclick = function () {
+		body.classList.remove('shown');
 		login.classList.remove('active');
-	}
+	};
 
 	login__exit.onclick = function() {
+		body.classList.remove('shown');
 		login.classList.remove('active');
 		login__form.reset();
-	}
+	};
 
-
-
+	
 }
