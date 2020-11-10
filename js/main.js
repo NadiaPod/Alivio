28e92456759ab95a1a6a38373a82eee35b30eb02
NadiaPod <72332868+NadiaPod@users.noreply.github.com>
2020-11-10 13:28:10 +0300

    Swipe menu
    Open/close swipe for mobile menu

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ window.onload = function(){
 	};
 
 	header__nav.onclick = function () {
-		body.classList.toggle('shown');
+		body.classList.remove('shown');
 		container.classList.remove('shown');
 		header__burger.classList.remove('shown');
 	    header__menu.classList.remove('shown');
@@ -28,12 +28,38 @@ window.onload = function(){
 	    header__menu.classList.remove('shown');
 	};
 
+	var touchPosition = new Object();
+	touchPosition.sX = 0;
+	touchPosition.eX = 0;
+	body.addEventListener("touchstart", function (e) { 
+		touchPosition.sX = e.touches[0].screenX;
+	 }, false);
+	// body.addEventListener("touchmove", function (e) { 
+	// 	touchPosition.eX = e.touches[0].screenX;
+	//  }, false);
+	body.addEventListener("touchmove", function (e) { 
+		touchPosition.eX = e.touches[0].screenX;
+		if(touchPosition.eX > touchPosition.sX){
+			body.classList.remove('shown');
+			container.classList.remove('shown');
+			header__burger.classList.remove('shown');
+	   		header__menu.classList.remove('shown');
+		}
+		else if(touchPosition.eX <= touchPosition.sX){
+			body.classList.add('shown');
+			container.classList.add('shown');
+	   		header__burger.classList.add('shown');
+	   		header__menu.classList.add('shown');
+		}
+	 }, false);
+
 	let login = document.querySelector('.login');
 	let login__form = document.querySelector('.login__form');
 	let login__btn = document.querySelector('.login__btn');
 	let login__exit = document.querySelector('.login__exit');
 
 	header__signIn.onclick = function(){
+		body.classList.remove('shown');
 		container.classList.remove('shown');
 		header__burger.classList.remove('shown');
 	    header__menu.classList.remove('shown');
