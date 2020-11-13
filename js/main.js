52c3a83a61abb80fb93f332669b6c45a1708bcba
NadiaPod <72332868+NadiaPod@users.noreply.github.com>
2020-11-13 03:35:20 +0300

    New feature
    New functional drawer + started improving adaptive.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ window.onload = function(){
 	let body = document.querySelector('body');
 	let container = document.querySelector('.container');
 	let header__burger = document.querySelector('.header__burger');
-	let header__menu = document.querySelector('.header__menu');
-	let header__nav = document.querySelector('.header__nav');
+	let header__menu = document.querySelector('.drawer');
+	let drawer__nav = document.querySelector('.drawer__nav');
 	let header__signIn = document.querySelector('.header__sign-in');
+	let drawer__signIn = document.querySelector('.drawer__sign-in');
+	let drawer__btn = document.querySelector('.drawer__btn');
 	let header__btn = document.querySelector('.header__btn');
 
 	header__burger.onclick = function(){
@@ -14,7 +16,7 @@ window.onload = function(){
 	    header__menu.classList.toggle('shown');
 	};
 
-	header__nav.onclick = function () {
+	drawer__nav.onclick = function () {
 		body.classList.remove('shown');
 		container.classList.remove('shown');
 		header__burger.classList.remove('shown');
@@ -28,37 +30,61 @@ window.onload = function(){
 	    header__menu.classList.remove('shown');
 	};
 
+	drawer__btn.onclick = function(){
+		body.classList.remove('shown');
+		container.classList.remove('shown');
+		header__burger.classList.remove('shown');
+	    header__menu.classList.remove('shown');
+	}
+
+	let drawerShown = document.querySelector('.drawer.shown');
 	var touchPosition = new Object();
 	touchPosition.sX = 0;
 	touchPosition.eX = 0;
-	body.addEventListener("touchstart", function (e) { 
+	var path = 0;
+	function startPos(e){
 		touchPosition.sX = e.touches[0].screenX;
-	 }, false);
-	// body.addEventListener("touchmove", function (e) { 
-	// 	touchPosition.eX = e.touches[0].screenX;
-	//  }, false);
-	body.addEventListener("touchmove", function (e) { 
+	}
+	function movePos(e){
 		touchPosition.eX = e.touches[0].screenX;
-		if(touchPosition.eX > touchPosition.sX){
-			body.classList.remove('shown');
+		path = touchPosition.eX - touchPosition.sX;
+		container.style.transform = `translateX(calc(-250px + ${path}px))`;
+		header__menu.style.transform = `translateX(calc(100vw + (${path}/2)px))`;
+	}
+	function moveEnd(e){
+		if(path > 100){
 			container.classList.remove('shown');
+			body.classList.remove('shown');
 			header__burger.classList.remove('shown');
-	   		header__menu.classList.remove('shown');
+			header__menu.classList.remove('shown');
+			container.removeAttribute('style');
+			header__menu.removeAttribute('style');
 		}
-		else if(touchPosition.eX <= touchPosition.sX){
-			body.classList.add('shown');
-			container.classList.add('shown');
-	   		header__burger.classList.add('shown');
-	   		header__menu.classList.add('shown');
+		else{
+			container.style.transform = "";
+			header__menu.style.transform = "";
+			container.removeAttribute('style');
+			header__menu.removeAttribute('style');
 		}
-	 }, false);
-
+	}
+	body.addEventListener("touchstart", startPos, false);
+	body.addEventListener("touchmove", movePos, false);
+	body.addEventListener("touchend", moveEnd, false);
+	
 	let login = document.querySelector('.login');
 	let login__form = document.querySelector('.login__form');
 	let login__btn = document.querySelector('.login__btn');
 	let login__exit = document.querySelector('.login__exit');
 
 	header__signIn.onclick = function(){
+		body.classList.remove('shown');
+		container.classList.remove('shown');
+		header__burger.classList.remove('shown');
+	    header__menu.classList.remove('shown');
+	    login.classList.toggle('active');
+	};
+
+	drawer__signIn.onclick = function(){
 		body.classList.remove('shown');
 		container.classList.remove('shown');
 		header__burger.classList.remove('shown');
